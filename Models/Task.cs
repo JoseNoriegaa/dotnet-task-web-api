113b388a3638bab32ce73ffa3d02e6ced0172a31
Jose Noriega <josenoriega723@gmail.com>
2024-06-12 09:34:18 -0700

    feat: add database connection

--- Models/Task.cs
+++ Models/Task.cs
@@ -3,22 +3,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EntityFrameworkPracticeApp.Models;
 
+[Table("tasks")]
 internal class Task
 {
     [Key]
+    [Column("id")]
     public Guid Id { get; set; }
 
     [Required]
     [MaxLength(200)]
+    [Column("name")]
     public string Name { get; set; } = "";
 
+    [Column("description")]
     public string Description { get; set; } = "";
 
+    [Column("priority")]
     public Priority Priority { get; set; }
 
     [ForeignKey("Id")]
+    [Column("category_id")]
     public Guid CategoryId { get; set; }
 
+    [Column("created_at")]
     public DateTime CreatedAt { get; set; }
 
     public virtual Category? Category { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Services
+    .AddNpgsql<TaskDBContext>(builder
+        .Configuration
+        .GetConnectionString("TasksDatabase"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/db-connection", static async ([FromServices] TaskDBContext dbCtx) =>
+{
+    var result = await dbCtx.Database.EnsureCreatedAsync();
+
+    return Results.Ok($"Database status (created, in-memory): {result}, {dbCtx.Database.IsNpgsql()}");
+});
 
 app.Run();
