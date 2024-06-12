d3660a0760c21156113f891b5bcc0936f33c4ec9
Jose Noriega <josenoriega723@gmail.com>
2024-06-12 10:23:39 -0700

    feat: switch to fluent api

--- Models/Category.cs
+++ Models/Category.cs
@@ -3,19 +3,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EntityFrameworkPracticeApp.Models;
 
-[Table("categories")]
 internal class Category
 {
-    [Key]
-    [Column("id")]
     public Guid Id { get; set; }
 
-    [Required]
-    [MaxLength(150)]
-    [Column("name")]
     public string Name { get; set; } = "";
 
-    [Column("description")]
     public string Description { get; set; } = "";
 
     public virtual ICollection<Task> Tasks { get; set; } = [];
--- TaskDBContext.cs
+++ TaskDBContext.cs
@@ -1,8 +1,69 @@
 using Microsoft.EntityFrameworkCore;
 
+using Models = EntityFrameworkPracticeApp.Models;
+
 internal class TaskDBContext: DbContext {
     public DbSet<EntityFrameworkPracticeApp.Models.Category> Categories {get; set; }
     public DbSet<EntityFrameworkPracticeApp.Models.Task> Tasks { get; set; }
 
     public TaskDBContext(DbContextOptions<TaskDBContext> options): base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Models.Category>(static model => {
+            model.ToTable("category");
+            model.HasKey(static p => p.Id);
+
+            model
+                .Property(static p => p.Id)
+                .HasColumnName("id");
+
+            model
+                .Property(static p => p.Name)
+                .IsRequired()
+                .HasMaxLength(150)
+                .HasColumnName("name");
+
+            model
+                .Property(static p => p.Description)
+                .HasColumnName("description");
+        });
+
+        modelBuilder.Entity<Models.Task>(static model => {
+            model.ToTable("task");
+
+            model
+                .Property(static p => p.Id)
+                .HasColumnName("id");
+
+            model
+                .Property(static p => p.Name)
+                .IsRequired()
+                .HasMaxLength(200)
+                .HasColumnName("name");
+
+            model
+                .Property(static p => p.Description)
+                .HasColumnName("description");
+
+            model
+                .Property(static p => p.Priority)
+                .HasColumnName("priority");
+
+            model
+                .Property(static p => p.CategoryId)
+                .HasColumnName("category_id");
+
+            model
+                .HasOne(static p => p.Category)
+                .WithMany(static p => p.Tasks)
+                .HasForeignKey(static p => p.CategoryId);
+
+            model
+                .Property(static p => p.CreatedAt)
+                .HasColumnName("created_at");
+
+            model.Ignore(static p => p.ShortDescription);
+        });
+    }
 }
