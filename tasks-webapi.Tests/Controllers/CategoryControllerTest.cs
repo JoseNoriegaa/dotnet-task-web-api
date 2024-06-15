15705086b80f489469bfe1b6fc104875790ea052
Jose Noriega <josenoriega723@gmail.com>
2024-06-15 10:35:48 -0700

    feat: unit test for CategoryController.Retrieve

--- tasks-webapi.Tests/Controllers/CategoryControllerTest.cs
+++ tasks-webapi.Tests/Controllers/CategoryControllerTest.cs
@@ -19,7 +19,7 @@ public class CategoryControllerTest
     {
         var data = CategoryDataMocks.Generate(2);
         var categoryService = new Mock<ICategoryService>();
-        categoryService.Setup(c => c.GetAllCategories()).Returns(data);
+        categoryService.Setup(static c => c.GetAllCategories()).Returns(data);
 
         var controller = new CategoryController(logger.Object, categoryService.Object);
         var result = controller.List();
@@ -30,5 +30,38 @@ public class CategoryControllerTest
         var category = returnValue.First();
         Assert.Equal(2, data.Count());
         Assert.Equal(category.Id, data.First().Id);
+    }
+
+    [Fact]
+    public void Retrieve_should_return_a_category()
+    {
+        var data = CategoryDataMocks.Generate(1);
+        var item = data.First();
+        var categoryService = new Mock<ICategoryService>();
+        categoryService.Setup(c => c.GetCategoryById(item.Id)).Returns(item);
+
+        var controller = new CategoryController(logger.Object, categoryService.Object);
+        var result = controller.Retrieve(item.Id);
+
+        Assert.NotNull(result);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsAssignableFrom<Category>(okResult.Value);
+        Assert.Equal(returnValue.Id, item.Id);
+    }
+
+    [Fact]
+    public void Retrieve_should_return_not_found()
+    {
+        var id = Guid.NewGuid();
+        var categoryService = new Mock<ICategoryService>();
+        categoryService.Setup(c => c.GetCategoryById(id));
+
+        var controller = new CategoryController(logger.Object, categoryService.Object);
+        var result = controller.Retrieve(id);
+
+        Assert.NotNull(result);
+        var okResult = Assert.IsType<NotFoundObjectResult>(result);
+        var returnValue = Assert.IsAssignableFrom<DTOs.ApiMessageDto>(okResult.Value);
+        Assert.Equal($"Category with ID '{id}' was not found", returnValue.Message);
     }
 }
