960b18454b68722c12e5bb8ada6f0aca5aab408a
Jose Noriega <josenoriega723@gmail.com>
2024-06-15 14:37:49 -0700

    feat: unit tests for TaskController.Retrieve

--- tasks-webapi.Tests/Controllers/TaskControllerTest.cs
+++ tasks-webapi.Tests/Controllers/TaskControllerTest.cs
@@ -31,4 +31,40 @@ public class TaskControllerTest
         Assert.Equal(2, data.Count());
         Assert.Equal(task.Id, data.First().Id);
     }
+
+    [Fact]
+    public void Retrieve_should_return_task()
+    {
+        var data = TaskDataMocks.Generate(1);
+        var item = data.First();
+        var taskService = new Mock<ITaskService>();
+        taskService.Setup(c => c.GetTaskById(item.Id)).Returns(item);
+
+        var controller = new TaskController(logger.Object, taskService.Object, categoryService.Object);
+        var result = controller.Retrieve(item.Id);
+
+        taskService.Verify(c => c.GetTaskById(item.Id), Times.Once);
+        Assert.NotNull(result);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsAssignableFrom<Models.Task>(okResult.Value);
+        Assert.Equal(returnValue.Id, item.Id);
+    }
+
+    [Fact]
+    public void Retrieve_should_return_not_found()
+    {
+        var data = TaskDataMocks.Generate(1);
+        var item = data.First();
+        var taskService = new Mock<ITaskService>();
+        taskService.Setup(c => c.GetTaskById(item.Id));
+
+        var controller = new TaskController(logger.Object, taskService.Object, categoryService.Object);
+        var result = controller.Retrieve(item.Id);
+
+        taskService.Verify(c => c.GetTaskById(item.Id), Times.Once);
+        Assert.NotNull(result);
+        var okResult = Assert.IsType<NotFoundObjectResult>(result);
+        var returnValue = Assert.IsAssignableFrom<DTOs.ApiMessageDto>(okResult.Value);
+        Assert.Equal($"Task with ID '{item.Id}' was not found", returnValue.Message);
+    }
 }
